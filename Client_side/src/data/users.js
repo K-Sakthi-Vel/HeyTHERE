3e99a28d217823202ddc6956962a250955b067c9
K SAKTHIVEL <klsakthi333@gmail.com>
2024-05-20 07:49:47 +0530

    Add files via upload

---
+++ Client_side/src/data/users.js
@@ -0,0 +1,222 @@
+export const userData=[
+    {
+        name:"Group Chat",
+        mobileNumber:"",
+        status:"",
+        convo:0,
+        lastSeen:"",
+        chat_type:"Group"
+
+    },
+    {
+        name:"Jerry Bassu",
+        mobileNumber:"2222222222",
+        status:"Miyyow",
+        ownMsg:["Hi","How are you","Yeah","I am good","Yeah"],
+        friendMsg:["Hii","I am good!!","How about you","Sounds great!!"],
+        convo:[
+            {username:"Jerry",friend:"Hi, i am jerry"},
+            {username:"Sakthi",you:"Hii jerry"},
+            {username:"Jerry",friend:"Where are you, right now"},
+            {username:"Sakthi",you:"Home"},
+            {username:"Jerry",friend:"Oh! great"},
+            {username:"Sakthi",you:"Could we meet!"},
+            {username:"Jerry",friend:"Yeah! sure"},
+            {username:"Sakthi",you:"Ok then! catch you at erode"},
+            {username:"Jerry",friend:"I started walking😂"},
+            {username:"Sakthi",you:"Me too"},
+            {username:"Jerry",friend:"Ok see you there!"}
+        ],
+        lastSeen:"11:30:PM",
+        chat_type:"Individual"
+    },
+    {
+        name:"Siva bassu",
+        mobileNumber:"3333333333",
+        status:"I am the king",
+        convo:[
+            {username:"Persu",friend:"Hi, i am perusu"},
+            {username:"Sakthi",you:"Bassovv"},
+            {username:"Persu",friend:"What's up!!"},
+            {username:"Sakthi",you:"Boring!!"},
+            {username:"Persu",friend:"Okk let's jump in bermuda😂"},
+            {username:"Sakthi",you:"Haha, meet you at lobby"},
+            {username:"Persu",friend:"I'm already in'"},
+            {username:"Sakthi",you:"Sounds great!!"},
+            {username:"Persu",friend:"Yeah quick"},
+            {username:"Sakthi",you:"On my way"}
+        ],
+        lastSeen:"07:30:AM",
+        chat_type:"Individual"
+    },
+    {
+        name:"Gana Bassu",
+        mobileNumber:"4444444444",
+        status:"Hard work pays off",
+        convo:[
+            {username:"Basey",friend:"Hi, i am bassu"},
+            {username:"Sakthi",you:"Bassoiee"},
+            {username:"Basey",friend:"How you doing!"},
+            {username:"Sakthi",you:"I am good!!"},
+            {username:"Basey",friend:"Great!"},
+            {username:"Sakthi",you:"How about you, boss"},
+            {username:"Basey",friend:"I am good too"},
+            {username:"Sakthi",you:"Sounds great!!"},
+            {username:"Basey",friend:"When you will come home"},
+            {username:"Sakthi",you:"Next weekend i would have 3 days leave"},
+            {username:"Sakthi",you:"Meet you next week bassey"},
+            {username:"Basey",friend:"Ok see you!!!"}
+        ],
+        lastSeen:"06:33:PM",
+        chat_type:"Individual"
+    },
+    {
+        name:"Mugi Siskkaa",
+        mobileNumber:"555555555",
+        status:"Doctor",
+        convo:[
+            {username:"Siska",friend:"Hi, i am akkov"},
+            {username:"Sakthi",you:"Hii akkovv"},
+            {username:"Siska",friend:"Did you seen siva"},
+            {username:"Sakthi",you:"Yes, i seen him at bermuda"},
+            {username:"Siska",friend:"Bermuda, are you kidding!!"},
+            {username:"Sakthi",you:"Seriously, i'm also with him right now"},
+            {username:"Siska",friend:"What you guys are doing in bermuda🙄"},
+            {username:"Sakthi",you:"Akkov, dont get serious😂"},
+            {username:"Sakthi",you:"The bermuda is not what you thing"},
+            {username:"Sakthi",you:"It's a map name in free fire😂"},
+            {username:"Siska",friend:"If i was there in the game, i will kill you both"},
+            {username:"Sakthi",you:"No, you wont we are pro's"},
+            {username:"Siska",friend:"Wait, i'm coming home😠"},
+            {username:"Sakthi",you:"🙄🤐"}
+        ],
+        lastSeen:"03:30:PM",
+        chat_type:"Individual"
+    },
+    {
+        name:"Manoj",
+        mobileNumber:"1111111111",
+        status:"DO OR DIE",
+        convo:[
+            {username:"Mano",friend:"Hi, i am manoj"},
+            {username:"Sakthi",you:"Hii"},
+            {username:"Mano",friend:"How are you"},
+            {username:"Sakthi",you:"I am good!!"},
+            {username:"Mano",friend:"Yeah"},
+            {username:"Sakthi",you:"How about you"},
+            {username:"Mano",friend:"I am good"},
+            {username:"Sakthi",you:"Sounds great!!"},
+            {username:"Mano",friend:"Yeah"},
+            {username:"Sakthi",you:"Hmmmm"}
+        ],
+        lastSeen:"12:00:AM",
+        chat_type:"Individual"
+    },
+    {
+        name:"Naveen Mottae",
+        mobileNumber:"6666666666",
+        status:"Vijay fan",
+        convo:[
+            {username:"Motae",friend:"Hi da sakthi"},
+            {username:"Sakthi",you:"Hii da mottae"},
+            {username:"Motae",friend:"Dei, climate seems overcast"},
+            {username:"Sakthi",you:"Yeah i seen that i did think of to call you"},
+            {username:"Motae",friend:"Ok could you come"},
+            {username:"Sakthi",you:"On my way da"},
+            {username:"Motae",friend:"Bring those bats at stumps"},
+            {username:"Sakthi",you:"Yeah i will"},
+            {username:"Motae",friend:"Ok quick can't wait"},
+            {username:"Sakthi",you:"I'm not coming"},
+            {username:"Motae",friend:"Haha, rather i will"},
+            {username:"Sakthi",you:"😁Ok ok started walking"}
+        ],
+        lastSeen:"05:43:PM",
+        chat_type:"Individual"
+    },
+    {
+        name:"Damsa kannan",
+        mobileNumber:"7777777777",
+        status:"Mejaash",
+        convo:[
+            {username:"Damsa",friend:"Hi da machan"},
+            {username:"Sakthi",you:"Hii damsaa"},
+            {username:"Damsa",friend:"Yow, did you watched yesterday match"},
+            {username:"Sakthi",you:"Yeah, it was a amazing match"},
+            {username:"Sakthi",you:"IND vs PAK"},
+            {username:"Damsa",friend:"We won, the more satisfying part is virats 100"},
+            {username:"Sakthi",you:"Yeah i felt the same"},
+            {username:"Damsa",friend:"Anyway, the entire match was great"},
+            {username:"Sakthi",you:"Yeah, especially on the batting side"},
+            {username:"Damsa",friend:"Yeah it was"},
+            {username:"Sakthi",you:"Okk let's wait for next game"},
+            {username:"Damsa",friend:"Yeah, let's see"}
+        ],
+        lastSeen:"08:55:AM",
+        chat_type:"Individual"
+    },
+    {
+        name:"Bendu Mookan",
+        mobileNumber:"8888888888",
+        status:"I am bendu mookan",
+        convo:[
+            {username:"Bendu",friend:"Hi da sakthi"},
+            {username:"Sakthi",you:"Dei, bendu boyy"},
+            {username:"Bendu",friend:"It's been 3 years we talked!!!"},
+            {username:"Sakthi",you:"OMG, days are running like a rocket"},
+            {username:"Bendu",friend:"Yes da"},
+            {username:"Sakthi",you:"Currently where you are working"},
+            {username:"Bendu",friend:"Texmo"},
+            {username:"Sakthi",you:"Sounds great!!"},
+            {username:"Bendu",friend:"Yeah"},
+            {username:"Sakthi",you:"When you will go hometown"},
+            {username:"Bendu",friend:"This week"},
+            {username:"Sakthi",you:"Ok, if i got a chance let's meet"},
+            {username:"Bendu",friend:"Yeah, sure da"}
+        ],
+        lastSeen:"12:44:AM",
+        chat_type:"Individual"
+    },
+    {
+        name:"Kolaveee",
+        mobileNumber:"9999999999",
+        status:"Evamleyy athu",
+        convo:[
+            {username:"Ravi",friend:"Hi da machi"},
+            {username:"Sakthi",you:"Hii da naai"},
+            {username:"Ravi",friend:"You still alive"},
+            {username:"Sakthi",you:"You are alive, why i don't😂"},
+            {username:"Ravi",friend:"🤕Hehehe"},
+            {username:"Sakthi",you:"Ok, what's the matter"},
+            {username:"Ravi",friend:"Did you seen that, IQOO released new device"},
+            {username:"Sakthi",you:"Yes da, the performance king i would say"},
+            {username:"Ravi",friend:"Yes da, all-rounder"},
+            {username:"Sakthi",you:"Price little high right"},
+            {username:"Ravi",friend:"But, worth for price"},
+            {username:"Sakthi",you:"Yes, are you buying that"},
+            {username:"Ravi",friend:"No, just 3 months before i bought a IQOO device"},
+            {username:"Sakthi",you:"Okk da"}
+        ],
+        lastSeen:"04:52:PM",
+        chat_type:"Individual"
+    },
+    {
+        name:"Barath Bassu",
+        mobileNumber:"1000000000",
+        status:"Life is easy",
+        convo:[
+            {username:"Barat",friend:"Hi da kannu"},
+            {username:"Sakthi",you:"Hii barath basseyy"},
+            {username:"Barat",friend:"How is your studies are going"},
+            {username:"Sakthi",you:"Going well bassey, currently building applications"},
+            {username:"Barat",friend:"Really!!"},
+            {username:"Sakthi",you:"Yes, i built 3 full-stack projects and 2 front-end. "},
+            {username:"Barat",friend:"Ohh that's great!!"},
+            {username:"Barat",friend:"If you got time send me those hosted app link"},
+            {username:"Sakthi",you:"Yeah i will"},
+            {username:"Barat",friend:"Okk, keep building applications"},
+            {username:"Sakthi",you:"Yess, bosss!"}
+        ],
+        lastSeen:"01:30:PM",
+        chat_type:"Individual"
+    }
+]
